81f9b5ba603545d3d824c5f202e710adafe7deb2
tesshasnochills <tess.nganga2020@gmail.com>
2023-03-07 15:31:14 +0300

    fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range (97,123)
-    if chr(letter) != 'q' and chr(letter) != 'e'
+    if chr (letter) != 'q' and chr (letter) != 'e'
         print("{}".format(chr(letter)), end="")

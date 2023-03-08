a17254c7cf46072564d6b52c9e53c35fcbe986d2
tesshasnochills <tess.nganga2020@gmail.com>
2023-03-08 22:12:09 +0300

    fixes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
+i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    letter = chr(c)
-    if c % 2 == 0:
-        letter = letter.upper()
-    else:
-        letter = letter.lower()
-    print(letter, end="")
-
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0

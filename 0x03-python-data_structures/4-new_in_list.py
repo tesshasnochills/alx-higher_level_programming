0f4fd14091421ef40b05ef3a3422ee7d7dd2b826
tesshasnochills <tess.nganga2020@gmail.com>
2023-03-10 17:56:28 +0300

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+        else:
+            new_list.append(element)
+    return new_list
+

e78199b690ba9b2ca6b988e32c94403a4861ccda
tesshasnochills <tess.nganga2020@gmail.com>
2023-03-10 10:16:01 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     while (len(argv) > 1):
         if (len(argv) == 2):
-            print("{} arguments:".format(len(argv) - 1))
+            print("{} argument:".format(len(argv) - 1))
             print("{}: {}".format(len(argv) - 1, argv[1]))
             exit()
         else:

c5a0b054ab3f1bfb0329497bafc3b54b85c22e37
tesshasnochills <tess.nganga2020@gmail.com>
2023-05-12 02:05:25 +0300

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    if (len(argv) < 2):
+        print("0")
+    elif (len(argv) == 2):
+        print(argv[1])
+    else:
+        dup = [int(argv[i]) for i in range(1, len(argv))]
+        Sum = sum(dup)
+        print(Sum)

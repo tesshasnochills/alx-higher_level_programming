7b63be86a48ae0d4643c4fb05a52ed1eb3fbaef5
tesshasnochills <tess.nganga2020@gmail.com>
2023-06-01 02:18:28 +0300

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
+"""This module contains the class Square
+"""
+
+
 class Square:
-    def __init__(self, size):
-        self.size = size
+    """This is a square class
+    """
 
-    def size(self):
-        return self.__size
-
-    def size(self, value):
-        if not isinstance(value, int):
+    def __init__(self, size=0):
+        """initializer function"""
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
     def area(self):
-        return self.__size * self.__size
+        """Returns the current square area"""
+        return self.__size ** 2
 
-    def my_print(self):
-        if not self.__size:
-            print("")
-        for i in range(self.__size):
-            print("#" * self.__size)
+    @property
+    def size(self):
+        """Getter method for the private property self
+
+        The setter method checks whether the value passed is an integer and
+        not less than 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")

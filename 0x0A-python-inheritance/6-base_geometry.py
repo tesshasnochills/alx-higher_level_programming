7e299e450a80146fcdd35275e3c6f30420a7a3d0
tesshasnochills <tess.nganga2020@gmail.com>
2023-06-13 11:45:14 +0300

    Improve Geomerty

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ a public instance method area"""
+    def area(self):
+        raise Exception("area() is not implemented")

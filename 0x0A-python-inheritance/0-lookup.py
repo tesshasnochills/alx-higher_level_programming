6ed8c86dbe51de95b4a6639ae4fe7a684d3c4a9d
tesshasnochills <tess.nganga2020@gmail.com>
2023-06-13 11:06:55 +0300

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+
+A lookup function
+"""
+
+
+def lookup(obj):
+    """
+
+    a function that returns the list of available attributes and
+    methods of an object
+    """
+
+    return (dir(obj))

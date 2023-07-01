ff2fcf1d1577fdbfc418b0e4c70c77091843ee3b
tesshasnochills <tess.nganga2020@gmail.com>
2023-07-01 16:12:41 +0300

    Tezt indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Defines a text indentation function"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each ',', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If the text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    t = 0
+    while t < len(text) and text[t] == ' ':
+        t += 1
+
+    while t < len(text):
+        print(text[t], end="")
+        if text[t] == "/n" or text[t] in ".?:":
+            if text[t] in ".?:":
+                print("/n")
+            t += 1
+            while t < len(text) and text[t] == ' ':
+                t += 1
+            continue
+        c += 1

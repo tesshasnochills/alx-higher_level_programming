f57ea262a890baf8176a99297068960ad29b7fef
tesshasnochills <tess.nganga2020@gmail.com>
2023-10-18 18:29:28 +0300

    advanced tasks

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- using left join and a where clause incase the genre id is null
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
